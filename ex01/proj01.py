efda753d2a60f79b5fd9cc9f9d18c1b16003d73f
Oren Samuel <orensam@gmail.com>
2014-10-31 02:08:12 +0200

    ex01 works

--- ex01/proj01.py
+++ ex01/proj01.py
@@ -8,9 +8,8 @@ class Tableau(object):
     point_dict.update({i:1 for i in xrange(17)})
     hands = ([1,2,3,4,5], [6,7,8,9,10], [11,12,13], [14,15,16], [1,6], [2,7,11,14], [3,8,12,15], [4,9,13,16], [5,10])
 
-    
     def __init__(self):
-        self.t = [Spot(i) for i in xrange(1,17)]
+        self.t = [Spot(i) for i in xrange(1,21)]
     
     def __str__(self):
         s = ""
@@ -19,7 +18,12 @@ class Tableau(object):
         s += "%9s %4s %4s"%tuple(self.t[10:13]) + '\n'
         s += "%9s %4s %4s"%tuple(self.t[13:16])
         return s
-    
+
+    def __getitem__(self, pos):
+        if isinstance(pos, slice):
+            return self.t[pos.start-1 : pos.stop-1]
+        return self.t[pos-1]
+
     def __setitem__(self, pos, card):
         self.t[pos - 1] = card
     
@@ -58,7 +62,7 @@ class Tableau(object):
         return self.calc_score_from_values(hand_values)        
 
     def gen_value_options(self, values):
-       	if len(values) == 0:
+        if len(values) == 0:
             yield []
         else:
             for val in self.gen_value_options(values[1:]):
@@ -76,8 +80,10 @@ class Tableau(object):
             return self.point_dict[tot]
     
     def to_values(self):
-        return [self.t[i].get_value() for i in xrange(len(self.t))]   
+        return [self.t[i].get_value() for i in xrange(17)]
 
+    def pos_full(self, pos):
+        return bool(self[pos])
             
                                                       
             
@@ -107,23 +113,59 @@ class Game(object):
         self.deck.shuffle()
         self.tableau = Tableau()
 
+    def  print_cards(self):
+        print "Tableau:"
+        print self.tableau
+        print
+        print "Discards: %4s %4s %4s %4s" % tuple(self.tableau[17:21])
+        print
+
     def play(self):
-        
-        self.tableau[1] = Card(1,'s')
-        self.tableau[2] = Card(8,'h')
-        self.tableau[6] = Card(1,'h')
-        self.tableau[7] = Card(8,'d')
-        print self.tableau.calc_score_simple()
-        print self.tableau.calc_score_advanced()
+
+        self.print_cards()
 
         while True:
-            break
-            stop = self.step()
-            if stop:
-                break
+            inp = raw_input("Choose an option: d - draw a card, s - simple calculation, a - advanced calculation, q - quit: ")
+            print
+            if inp in "qQ":
+                break;
+            elif inp in "dD":
+                self.play_step()
+                self.print_cards()
+            elif inp in "sS":
+                print "The total score (simple algorithm) is: %2s" % self.tableau.calc_score_simple()
+            elif inp in "aA":
+                print "The total score (advanced algorithm) is: %2s" % self.tableau.calc_score_advanced()
     
-    def step(self):
-        pass    
+    def play_step(self):
+        card = self.deck.deal()
+        print "Card Dealt: %4s" %card
+        pos = self.get_pos_from_user()
+        self.tableau[pos] = card
+
+    def get_pos_from_user(self):
+
+        while True:
+
+            inp = raw_input("Choose location (1 - 20) to place the new card: ")
+
+            try:
+                pos = int(inp)
+            except ValueError:
+                print "Error: input is not an integer"
+                continue
+
+            if pos < 1 or pos > 20:
+                print "Error: input is out of range"
+                continue
+
+            if self.tableau.pos_full(pos):
+                print "Error: a card was already placed in this spot"
+                continue
+
+            return pos
+
+
             
 
 if __name__=="__main__":
